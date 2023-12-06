7a1f6c4c2b55a122b7bd628442c659be3abecdd8
soongruijie <r.j.soong@lse.ac.uk>
2023-12-06 13:43:50 +0000

    created-dees-package

--- dees_package/genius_functions.py
+++ dees_package/genius_functions.py
@@ -1,5 +1,3 @@
-from scrapy import Selector
-
 def generate_song_url(song_artist, song_title):
     '''
     Returns a string of the URL for the Genius page of the song
@@ -11,27 +9,45 @@ def generate_song_url(song_artist, song_title):
         Returns:
             song_url (str): The URL for the Genius page of the song
     '''
+    
     base_url = 'https://genius.com/'
     
+    # format the artist name and song title
+    song_artist = song_artist.replace('&', 'and')
     formatted_artist = song_artist.lower().replace(' ', '-')
     formatted_title = song_title.lower().replace(' ', '-')
     
+    # generate the song URL by concatenating strings according to Genius formatting
     song_url = f'{base_url}{formatted_artist}-{formatted_title}-lyrics'
 
     return song_url
+
+import requests
+from scrapy import Selector
+import re
 
 def scrape_lyrics(session, song_url):
     '''
     Returns a string of song lyrics, with each line separated by a new line
 
         Parameters:
+            session (variable): The session that has been initialised for requesting from the Genius website
             song_url (str): The URL of the Genius page for the song
 
         Returns:
             lyrics (str): The lyrics of the song
     '''
+    
+    # use initialised session to enhance performance
     response = session.get(song_url)
     sel = Selector(text=response.text)
-    lyrics = '\n'.join(sel.css('div.Lyrics__Container-sc-1ynbvzw-1.kUgSbL ::text').getall())
+    
+    # scrape lyrics into one large string
+    raw_lyrics = ' '.join(sel.css('div.Lyrics__Container-sc-1ynbvzw-1.kUgSbL ::text').getall())
+
+    # clean lyrics using regular expression to remove words in square brackets
+    pattern = r'\[.*?\]'
+    result_string = re.sub(pattern, '', raw_lyrics)
+    lyrics = ' '.join(result_string.split())
 
     return lyrics
