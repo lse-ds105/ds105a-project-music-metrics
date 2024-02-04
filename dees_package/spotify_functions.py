e740932fd2de611c4cf5374359978a4e89dd101f
ruikai30 <liuruikai30@gmail.com>
2024-02-04 11:33:30 +0000

    update

--- dees_package/spotify_functions.py
+++ dees_package/spotify_functions.py
@@ -1,30 +1,34 @@
+import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import spotipy as sp
 
-def get_release_date(song, client_id, client_secret):
-    client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+def get_release_date(song):
     result = sp.search(song)
-    release_date = result['tracks']['items'][0]['album']['release_date']
-    return release_date
+    tracks = result['tracks']['items']
+    if len(tracks) > 0:
+        return tracks[0]['album']['release_date']
+    else:
+        return None
 
-def get_popularity(song, client_id, client_secret):
-    client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+def get_popularity(song):
     result = sp.search(song)
-    popularity = result['tracks']['items'][0]['popularity']
-    return popularity
+    tracks = result['tracks']['items']
+    if len(tracks) > 0:
+        return tracks[0]['popularity']
+    else:
+        return None
 
-def get_explicitness(song, client_id, client_secret):
-    client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+def get_explicitness(song):
     result = sp.search(song)
-    explicitness = result['tracks']['items'][0]['explicit']
-    return explicitness
+    tracks = result['tracks']['items']
+    if len(tracks) > 0:
+        return tracks[0]['explicit']
+    else:
+        return None
 
-def get_market_number(song, client_id, client_secret):
-    client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+def get_market_number(song):
     result = sp.search(song)
-    available_markets = result['tracks']['items'][0]['available_markets']
-    return len(available_markets)
+    tracks = result['tracks']['items']
+    if len(tracks) > 0:
+        return len(tracks[0]['available_markets'])
+    else:
+        return None
